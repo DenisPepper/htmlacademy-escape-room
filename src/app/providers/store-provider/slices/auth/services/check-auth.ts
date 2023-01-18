3ca5ed4185cd55d95e4486df2e128b5e0bccc0f3
DenisPepper <thech@yandex.ru>
2023-01-18 10:14:38 +0300

    added check-auth async thunk

---
+++ src/app/providers/store-provider/slices/auth/services/check-auth.ts
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import {getToken} from '../../../../../../shared/lib/token/token';
+import {LoginData} from '../types/auth-schema';
+import {HttpErrorMessage} from '../../../../../../shared/http-client/http-client';
+
+export const CheckAuth = createAsyncThunk<LoginData, undefined, {rejectValue: string}>(
+  'CHECK_AUTH',
+  async (_args, thunkAPI) => {
+    try {
+      const response = await axios.get<LoginData>(
+        'https://grading.design.pages.academy/escape-room/login',
+        {headers: {'x-token': getToken()}});
+      // eslint-disable-next-line no-console
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(HttpErrorMessage.ON_CHECK_AUTH_REJECTED);
+    }
+  }
+);
