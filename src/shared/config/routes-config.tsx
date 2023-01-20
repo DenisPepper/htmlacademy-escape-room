465261ff163a2455b8d59d1a679885570a32d0cb
DenisPepper <thech@yandex.ru>
2023-01-20 12:24:26 +0300

    fix path to css in index.html

--- src/shared/config/routes-config.tsx
+++ src/shared/config/routes-config.tsx
@@ -2,7 +2,7 @@ export const enum AppRoutes {
   Main ='/',
   Auth = '/auth',
   Contact = '/contact',
-  Quest = '/quest',
+  Quest = '/quest/:id',
   MyQuests = '/my-quests',
   Booking = '/booking',
   NotFound = '/*',
